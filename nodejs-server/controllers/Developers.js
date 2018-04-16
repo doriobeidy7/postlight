e1e071089a01b29d5297a899e4d1b4f15593e1bc
dori obeidy <doriobeidy@gmail.com>
2018-04-16 13:37:12 +0300

    adding node js server created with swagger tool

---
+++ nodejs-server/controllers/Developers.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var utils = require('../utils/writer.js');
+var Developers = require('../service/DevelopersService');
+
+module.exports.addEmployee = function addEmployee (req, res, next) {
+  var employee = req.swagger.params['employee'].value;
+  Developers.addEmployee(employee)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.deleteEmployee = function deleteEmployee (req, res, next) {
+  var id = req.swagger.params['id'].value;
+  Developers.deleteEmployee(id)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.listingEmployees = function listingEmployees (req, res, next) {
+  var page_id = req.swagger.params['page_id'].value;
+  var limit = req.swagger.params['limit'].value;
+  Developers.listingEmployees(page_id,limit)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.searchEmployees = function searchEmployees (req, res, next) {
+  var name = req.swagger.params['name'].value;
+  var title = req.swagger.params['title'].value;
+  var department = req.swagger.params['department'].value;
+  var location = req.swagger.params['location'].value;
+  Developers.searchEmployees(name,title,department,location)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.updateEmployee = function updateEmployee (req, res, next) {
+  var id = req.swagger.params['id'].value;
+  var employee = req.swagger.params['employee'].value;
+  Developers.updateEmployee(id,employee)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
---
+++ nodejs-server/service/DevelopersService.js
@@ -0,0 +1,117 @@
+'use strict';
+
+
+/**
+ * adds an employee
+ * Adds an employee to the system
+ *
+ * employee Employee employee info to add (optional)
+ * no response value expected for this operation
+ **/
+exports.addEmployee = function(employee) {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * delete an employee
+ * Delete an employee in the system
+ *
+ * id String pass employee id to delete a record
+ * no response value expected for this operation
+ **/
+exports.deleteEmployee = function(id) {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * listing employees
+ * By passing in the appropriate options, you can get a list of available employee in the system
+ *
+ * page_id String pass an optional page id for looking up records (optional)
+ * limit Integer maximum number of records to return (optional)
+ * returns List
+ **/
+exports.listingEmployees = function(page_id,limit) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = [ {
+  "emp_image" : "https://wd.imagelocation.com",
+  "name" : "Joe",
+  "location" : "beirut",
+  "id" : "d290f1ee-6c54-4b01-90e6-d701748f0851",
+  "title" : "Senior developer",
+  "department" : "Mobiles"
+}, {
+  "emp_image" : "https://wd.imagelocation.com",
+  "name" : "Joe",
+  "location" : "beirut",
+  "id" : "d290f1ee-6c54-4b01-90e6-d701748f0851",
+  "title" : "Senior developer",
+  "department" : "Mobiles"
+} ];
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * search employees
+ * By passing in the appropriate options, you can search for available employee in the system
+ *
+ * name String pass a name for looking up in records (optional)
+ * title String pass a title for looking up in records (optional)
+ * department String pass a department for looking up in records (optional)
+ * location String pass a location for looking up in records (optional)
+ * returns List
+ **/
+exports.searchEmployees = function(name,title,department,location) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = [ {
+  "emp_image" : "https://wd.imagelocation.com",
+  "name" : "Joe",
+  "location" : "beirut",
+  "id" : "d290f1ee-6c54-4b01-90e6-d701748f0851",
+  "title" : "Senior developer",
+  "department" : "Mobiles"
+}, {
+  "emp_image" : "https://wd.imagelocation.com",
+  "name" : "Joe",
+  "location" : "beirut",
+  "id" : "d290f1ee-6c54-4b01-90e6-d701748f0851",
+  "title" : "Senior developer",
+  "department" : "Mobiles"
+} ];
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * update an employee
+ * Update an employee in the system
+ *
+ * id String pass employee id to update a record
+ * employee Employee employee info to udpate (optional)
+ * no response value expected for this operation
+ **/
+exports.updateEmployee = function(id,employee) {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
