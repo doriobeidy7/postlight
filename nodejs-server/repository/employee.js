2f30e29137a5c6e662a58f033e50b2e3f3812912
dori obeidy <doriobeidy@gmail.com>
2018-04-18 12:16:36 +0300

    create update query

--- nodejs-server/repository/employee.js
+++ nodejs-server/repository/employee.js
@@ -1,14 +1,24 @@
 const pool = require('./connection');
 module.exports.getEmployees = function(callback) {
-  pool.query("Select * from employees", callback);
+  pool.query("SELECT * from employees ORDER BY id ASC", callback);
 }
 
 module.exports.setEmployee = function(callback, valuesData) {
 console.log(valuesData);
   const query = {
-    text: 'insert INTO employees(name, title, department, location) VALUES($1, $2, $3, $4)',
+    text: 'INSERT INTO employees(name, title, department, location) VALUES($1, $2, $3, $4)',
     values: valuesData
   }
   // callback
   pool.query(query, callback);
 }
+
+module.exports.updateEmployee = function(callback, valuesData) {
+  console.log(valuesData);
+    const query = {
+      text: 'UPDATE employees SET name=($1), title=($2), deparment=($3), location=($4)',
+      values: valuesData
+    }
+    // callback
+    pool.query(query, callback);
+  }
